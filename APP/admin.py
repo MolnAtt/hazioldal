fa395d05775782dc1a5b92ae6b4f14077e16aa8b
Molnár Attila <molnar.attila@szlgbp.hu>
2022-02-04 19:04:21 +0100

    felhasználói adatbázis

--- APP/admin.py
+++ APP/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Bigyo, Felhasznalo
+from .models import Bigyo
 
 admin.site.register(Bigyo)
-admin.site.register(Felhasznalo)
 
 # a trükkös admin-funkciókról, függvényekről az szlgbp_ma_heroku gitrepoban vannak jó példák.
+
--- APP/models.py
+++ APP/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 class Bigyo(models.Model):
@@ -14,13 +14,25 @@ class Bigyo(models.Model):
         return self.szoveg
 
 
-class Felhasznalo(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ujtulajdonsag = models.CharField(max_length=100)
-
+class Tanit(models.Model):
+    tanar = models.ForeignKey(User, on_delete=models.CASCADE)
+    csoport = models.ForeignKey(Group, on_delete=models.CASCADE)
+    
     class Meta:
-        verbose_name = 'Felhasználó'
-        verbose_name_plural = 'Felhasználók'
+        verbose_name = 'Tanár-Csoport reláció'
+        verbose_name_plural = 'Tanár-Csoport relációk'
 
     def __str__(self):
-        return str(self.user) + f' ({self.ujtulajdonsag})'
+        return f'{self.tanar} --- {self.csoport}'
+
+
+class Mentoral(models.Model):
+    mentor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentor')
+    mentoree = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentoree')
+    
+    class Meta:
+        verbose_name = 'Mentorálás'
+        verbose_name_plural = 'Mentorálás'
+
+    def __str__(self):
+        return f'{self.mentor} --- {self.mentoree}'
