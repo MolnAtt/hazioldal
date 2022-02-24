1dd198020904cb67d2636b9a5ffa2a78380b5606
Molnár Attila <molnar.attila@szlgbp.hu>
2022-02-24 20:10:16 +0100

    corsheaders apihoz

--- PROJEKT/settings.py
+++ PROJEKT/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'APP.apps.AppConfig',
 ]
 
@@ -31,6 +32,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -38,6 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True  
 
 ROOT_URLCONF = 'PROJEKT.urls'
 
