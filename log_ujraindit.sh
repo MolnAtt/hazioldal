282d7fc79ef18a3f4ab418b5daf52aeaf3970d04
Molnar Attila <molnar.attila@szlgbp.hu>
2022-10-09 22:21:26 +0000

    ez már nem kell

---
+++ log_ujraindit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git pull origin main
+python3 manage.py collectstatic --noinput --clear
+sudo systemctl restart gunicorn --
+sudo systemctl status gunicorn
+sudo nginx -t && sudo systemctl restart nginx
+sudo systemctl status nginx
+
+sudo journalctl -u gunicorn
+
