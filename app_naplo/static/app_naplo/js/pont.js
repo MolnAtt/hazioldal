21f410f8e955fbb59c04aa99eee5e5520b77dc02
Molnár Attila <molnar.attila@szlgbp.hu>
2023-02-26 14:17:37 +0100

    id egyszerűsítés

--- app_naplo/static/app_naplo/js/pont.js
+++ app_naplo/static/app_naplo/js/pont.js
@@ -7,10 +7,10 @@ main();
 
 function main(){
     document.addEventListener('keydown', tabkatt);
-    let osztalyinput = document.querySelector('#osztaly_name');
-    osztaly_name = osztalyinput.value;
-    let dolgozatinput = document.querySelector('#dolgozat_slug');
-    dolgozat_slug = dolgozatinput.value;
+    // let osztalyinput = document.querySelector('#osztaly_name');
+    // osztaly_name.value;
+    // let dolgozatinput = document.querySelector('#dolgozat_slug');
+    // dolgozat_slug.value;
 }
 
 function tabkatt(event){
@@ -28,7 +28,26 @@ async function pont_mentese(elem){
     let i_tanulo = par[0];
     let j_feladat = par[1];
 
-    let url = `${window.location.origin}/naplo/api/post/pont/write/${osztaly_name}/${dolgozat_slug}/`;
+    let url = `${window.location.origin}/naplo/api/post/pont/write/${osztaly_name.value}/${dolgozat_slug.value}/`;
+    let szotar = { 
+        'i_tanulo': i_tanulo,
+        'j_feladat': j_feladat,
+        'ertek':elem.value,
+    };
+    let res = await kuldo_fetch(url, szotar);
+    console.log(res);
+}
+
+
+////////////////////////////////
+// PONT API
+
+async function pont_mentese(elem){
+    let par = elem.id.split("-");
+    let i_tanulo = par[0];
+    let j_feladat = par[1];
+
+    let url = `${window.location.origin}/naplo/api/post/pont/write/${osztaly_name.value}/${dolgozat_slug.value}/`;
     let szotar = { 
         'i_tanulo': i_tanulo,
         'j_feladat': j_feladat,
