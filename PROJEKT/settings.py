5b48ef58a82275b7cca5aaaced98b07bda25b7fe
Molnár Attila <molnar.attila@szlgbp.hu>
2023-08-27 17:13:59 +0200

    file logging

--- PROJEKT/settings.py
+++ PROJEKT/settings.py
@@ -180,3 +180,36 @@ EMAIL_USE_TLS = local_settings.EMAIL_USE_TLS
 DEFAULT_FROM_EMAIL = local_settings.DEFAULT_FROM_EMAIL
 SERVER_EMAIL = local_settings.DEFAULT_FROM_EMAIL
 
+# settings.py
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'APP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
