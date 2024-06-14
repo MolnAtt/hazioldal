68115798026222dca3e284567fbd15cc32c8f4a3
Molnár Attila <60673392+MolnAtt@users.noreply.github.com>
2024-06-14 17:52:14 +0200

    lezárás js

---
+++ app_naplo/static/app_naplo/js/csoportnaplo.js
@@ -0,0 +1,38 @@
+document.addEventListener('keydown', tabkatt);
+
+function tabkatt(event){
+    let code = event.keyCode || event.which;
+    if (code === 9) {
+        if (event.target.classList.contains('jegy')){
+            jegy_lezaras_mentese(event.target);
+        } else if (event.target.classList.contains('szoveg')){
+            szoveges_ertekeles_mentese(event.target);
+        }
+    }
+}
+
+
+
+
+async function jegy_lezaras_mentese(elem){
+    let url = `${window.location.origin}/naplo/api/post/lezaras/write/jegy/${osztaly_name.value}/`;
+    let szotar = { 
+        'sorszam': elem.dataset.sorszam,
+        'ertek':elem.value,
+    };
+
+    let res = await kuldo_fetch(url, szotar);
+    console.log(res);
+}
+
+async function szoveges_ertekeles_mentese(elem){
+    let url = `${window.location.origin}/naplo/api/post/lezaras/write/szoveg/${osztaly_name.value}/`;
+    let szotar = { 
+        'sorszam': elem.dataset.sorszam,
+        'ertek':elem.value,
+    };
+
+    let res = await kuldo_fetch(url, szotar);
+    console.log(res);
+}
+
