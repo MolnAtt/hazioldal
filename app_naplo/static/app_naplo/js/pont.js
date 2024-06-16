47de1b313f93d4f31d18daf8ded95a4981e2a68d
MolnarAttilaNetbook <molnar.attila@szlgbp.hu>
2024-06-16 08:34:16 +0200

    not

--- app_naplo/static/app_naplo/js/pont.js
+++ app_naplo/static/app_naplo/js/pont.js
@@ -52,12 +52,12 @@ function szomszed_kijelolese(td,billentyu){
             break;
         case 'Enter':
         case 'ArrowDown':
-            if (td.parentElement.nextElementSibling.classList.contains('feladatmaximumok')){
+            if (!td.parentElement.nextElementSibling.classList.contains('feladatmaximumok')){
                 td.parentElement.nextElementSibling.children[oszlopindex(td)].firstElementChild.select();
             }
             break;
         case 'ArrowUp':
-            if (td.parentElement.previousElementSibling.classList.contains('feladatnevek')){
+            if (!td.parentElement.previousElementSibling.classList.contains('feladatnevek')){
                 td.parentElement.previousElementSibling.children[oszlopindex(td)].firstElementChild.select();
             }
             break;
