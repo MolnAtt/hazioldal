93e93320ef46dcde8e49384eb4682a3441b0888b
MolnarAttilaNetbook <molnar.attila@szlgbp.hu>
2024-06-16 08:15:40 +0200

    fel-le errorok keresése

--- app_naplo/static/app_naplo/js/pont.js
+++ app_naplo/static/app_naplo/js/pont.js
@@ -52,11 +52,13 @@ function szomszed_kijelolese(td,billentyu){
             break;
         case 'Enter':
         case 'ArrowDown':
+            console.log(td.parentElement.nextElementSibling);
             if (td.parentElement.nextElementSibling){
                 td.parentElement.nextElementSibling.children[oszlopindex(td)].firstElementChild.select();
             }
             break;
         case 'ArrowUp':
+            console.log(td.parentElement.previousElementSibling);
             if (td.parentElement.previousElementSibling){
                 td.parentElement.previousElementSibling.children[oszlopindex(td)].firstElementChild.select();
             }
